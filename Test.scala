026ba41fbc4183c92052d30bbcdc23d96b8adfa5
Eugene Burmako <xeno.by@gmail.com>
2015-03-14 22:12:16 +0100

    and here's how it works (YAY!)
    22:10 ~/Projects/scaladays2015 (macros)$ sbt run
    [info] Set current project to scaladays2015 (in build file:/Users/xeno_by/Projects/scaladays2015/)
    [info] Compiling 1 Scala source to /Users/xeno_by/Projects/scaladays2015/target/scala-2.11/classes...
    [info] Running Test
    {"head": 1, "tail": {"head": 2, "tail": {$tag: 1}, $tag: 0}, $tag: 0}
    [success] Total time: 14 s, completed Mar 14, 2015 10:11:40 PM

--- Test.scala
+++ Test.scala
@@ -6,7 +6,26 @@ package adt {
   trait Adt[T]
   object Adt {
     implicit def materialize[T]: Adt[T] = macro {
-      ???
+      T match {
+        case ref: Type.Ref =>
+          def validateLeaf(leaf: Member) = {
+            if (!leaf.isFinal) abort(s"${leaf.name} is not final")
+            if (!leaf.isCase) abort(s"${leaf.name} is not sealed")
+            if (!leaf.tparams.isEmpty) abort(s"${leaf.name} is not monomorphic")
+          }
+          val defn = ref.defn
+          if (defn.isClass || defn.isObject) {
+            validateLeaf(defn)
+          } else if (defn.isTrait) {
+            if (defn.isSealed) defn.children.foreach(validateLeaf)
+            else abort(s"${defn.name} is not sealed")
+          } else {
+            abort(s"unsupported ref to ${defn.name}")
+          }
+          q"new Adt[$T]{}"
+        case _ =>
+          abort(s"unsupported type $T")
+      }
     }
   }
 }
@@ -18,7 +37,52 @@ package serialization {
 
   object Serializer {
     implicit def materialize[T: adt.Adt]: Serializer[T] = macro {
-      ???
+      T match {
+        case ref: Type.Ref =>
+          val defn = ref.defn
+          if (defn.isClass || defn.isTrait || defn.isObject) {
+            val serializer = Term.fresh(defn.name + "Serializer")
+            val input = Term.fresh("input")
+            val body = {
+              def serializer(defn: Member, input: Term.Name, tagged: Boolean) = {
+                val fields = defn.ctor.params.map(x => x.field)
+                var entries: Seq[Term] = fields.map { field =>
+                  q""" "\"" + ${field.name.toString} + "\": " + serialize($input.${field.name}) """
+                }
+                if (tagged) {
+                  val tag = defn.parents.head.children.indexOf(defn).toString
+                  entries = entries :+ q""" "$$tag: " + $tag """
+                }
+                val unwrappedResult = entries.foldLeft(None: Option[Term]) { (acc, curr) =>
+                  acc.map(acc => q"""$acc + ", " + $curr""").orElse(Some(curr))
+                }
+                val contents = unwrappedResult.getOrElse(q""" "" """)
+                q""" "{" + $contents + "}" """
+              }
+              if (defn.isClass) {
+                serializer(defn, input, tagged = false)
+              } else if (defn.isObject) {
+                serializer(defn, input, tagged = false)
+              } else if (defn.isTrait) {
+                val refined = Pat.fresh("input")
+                val clauses = defn.children.map(leaf => p"case $refined: ${leaf.tpe.pat} => ${serializer(leaf, refined.name, tagged = true)}")
+                q"$input match { ..$clauses }"
+              } else {
+                abort(s"unsupported ref to ${defn.name}")
+              }
+            }
+            q"""
+              implicit object $serializer extends Serializer[$T] {
+                def apply($input: $T): String = $body
+              }
+              $serializer
+            """
+          } else {
+            abort(s"unsupported ref to ${defn.name}")
+          }
+        case _ =>
+          abort(s"unsupported type $T")
+      }
     }
     implicit def intSerializer: Serializer[Int] = new Serializer[Int] { def apply(x: Int) = x.toString }
     implicit def stringSerializer: Serializer[String] = new Serializer[String] { def apply(x: String) = x }
